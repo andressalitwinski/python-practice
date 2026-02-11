a7c320f628df89edeea15577bc0f28135a9b015f
Andressa Litwinski <andressalitwinski@gmail.com>
2026-02-11 10:37:23 +0000

    4 - Guessing Game - Refactoring

--- game.py
+++ game.py
@@ -1,29 +1,29 @@
 import random
 
+
 def main():
-    while True:
-        try:
-            n = int(input("Level: "))
-            if n > 0:
-                break
-        except ValueError:
-            pass
-    
-    number = random.randint(1, n)
+
+    level = get_positive_int("Level: ")
+    number = random.randint(1, level)
 
     while True:
+        g = get_positive_int("Guess: ")
+
+        if g < number:
+            print("Too small!")
+        elif g > number:
+            print("Too large!")
+        else:
+            print("Just right!")
+            break
+
+
+def get_positive_int(msg):
+    while True:
         try:
-            g = int(input("Guess: "))
-            if g > 0:
-                if g < number:
-                    print("Too small!")
-                    pass
-                if g > number:
-                    print("Too large!")
-                    pass
-                if g == number:
-                    print("Just right!")
-                    break
+            n = int(input(msg))
+            if n > 0:
+                return n
         except ValueError:
             pass
 
